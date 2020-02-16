c0e3b57bc4a8823a1a088f6d58f30eb394203e89
mhandel1955 <mhandel1955@gmail.com>
2020-02-15 23:14:20 -0500

    End of Lesson 17

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./styles.css";
 import BookContextProvider from "./contexts/BookContext";
 import Navbar from "./components/Navbar";
+import BookList from "./components/BookList";
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <Navbar />
+        <BookList />
       </BookContextProvider>
     </div>
   );
--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -6,7 +6,7 @@ export const BookContext = createContext();
 const BookContextProvider = props => {
   const [books, setBooks] = useState([
     { title: "name of the wind", author: "patrick rothfuss", id: 1 },
-    { title: "name of the wind", author: "patrick rothfuss", id: 1 }
+    { title: "the final empire", author: "brandon sanderson", id: 2 }
   ]);
 
   /*  in setbooks below {title, author, ....} if written
