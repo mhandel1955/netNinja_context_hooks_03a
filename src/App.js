1831e8eadacef9ed5de9799661890fb28ccb8d14
mhandel1955 <mhandel1955@gmail.com>
2020-02-15 22:22:54 -0500

    End of Lesson #16

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./styles.css";
 import BookContextProvider from "./contexts/BookContext";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <BookContextProvider />
+      <BookContextProvider>
+        <Navbar />
+      </BookContextProvider>
     </div>
   );
 }
